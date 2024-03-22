20abe639707733f9b907d4a193c3ac0306c3dcb1
Michael <machado.m3@icloud.com>
2024-03-22 14:31:33 -0300

    feat: appointment service

---
+++ Domains/Appointments/AppoitmentService.cs
@@ -0,0 +1,56 @@
+using BookingAnExperience.Resources.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingAnExperience.Domains.Appointments
+{
+    public interface IAppointmentService
+    {
+        Task<List<Appointment>> GetAllAsync();
+        Task<Appointment> GetByIdAsync(Guid id);
+        Task AddAsync(Appointment entity);
+        Task UpdateAsync(Appointment entity);
+        Task DeleteAsync(Guid id);
+    }
+
+    public class AppointmentService : IAppointmentService
+    {
+        private readonly BookingAnExperienceContext _context;
+
+        public AppointmentService(BookingAnExperienceContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Appointment>> GetAllAsync()
+        {
+            return await _context.Appointments.ToListAsync();
+        }
+
+        public async Task<Appointment> GetByIdAsync(Guid id)
+        {
+            return await _context.Appointments.FindAsync(id);
+        }
+
+        public async Task AddAsync(Appointment newAppointment)
+        {
+            _context.Appointments.Add(newAppointment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Appointment updatedAppointment)
+        {
+            _context.Entry(updatedAppointment).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var person = await _context.Appointments.FindAsync(id);
+            if (person is not null)
+            {
+                _context.Appointments.Remove(person);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- Resources/Contexts/DbContext.cs
+++ Resources/Contexts/DbContext.cs
@@ -4,12 +4,12 @@ using BookingAnExperience.Domains.Localizations;
 using BookingAnExperience.Domains.Users;
 using Microsoft.EntityFrameworkCore;
 
-namespace BookingAnExperience.Resources.Context
+namespace BookingAnExperience.Resources.Contexts
 {
     public class BookingAnExperienceContext : DbContext
     {
         public DbSet<User> Users { get; set; }
-        public DbSet<Appointment> Appoitments { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
         public DbSet<Localization> Localizations { get; set; }
         public DbSet<Home> Homes { get; set; }
 
